5241bd508acc1c6c8828abca23fb861fa76b1deb
teofilovatea <73780831+teofilovatea@users.noreply.github.com>
2023-04-05 19:58:10 +0200

    New project setup

---
+++ src/main/java/mk/ukim/finki/brainboost/BrainBoostApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.brainboost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainBoostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainBoostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mk/ukim/finki/brainboost/BrainBoostApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.brainboost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainBoostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
