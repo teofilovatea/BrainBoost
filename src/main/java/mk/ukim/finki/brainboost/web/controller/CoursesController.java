de326db314520e31d6163a98ea0b15156ad47d98
IvanaGeorgievaa <ivana.georgieva@students.finki.ukim.mk>
2023-04-19 14:23:04 +0200

    Added list all method in the Course Controller

--- src/main/java/mk/ukim/finki/brainboost/web/controller/CoursesController.java
+++ src/main/java/mk/ukim/finki/brainboost/web/controller/CoursesController.java
@@ -1,14 +1,33 @@
 package mk.ukim.finki.brainboost.web.controller;
 
+import mk.ukim.finki.brainboost.domain.Course;
+import mk.ukim.finki.brainboost.service.CourseService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/all_courses")
 public class CoursesController {
+    private final CourseService courseService;
+
+    public CoursesController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
     @GetMapping
-    public String getHomePage() {
+    public String getProductPage(@RequestParam(required = false) String error, Model model) {
+        if (error != null && !error.isEmpty()) {
+            model.addAttribute("hasError", true);
+            model.addAttribute("error", error);
+        }
+        List<Course> products = this.courseService.listAll();
+        model.addAttribute("products", products);
         return "all_courses";
     }
+
 }
