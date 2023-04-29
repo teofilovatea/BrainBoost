8bee996bd89b576256c062455f2ddd9e91d92a0e
IvanaGeorgievaa <ivana.georgieva@students.finki.ukim.mk>
2023-04-29 19:51:20 +0200

    Added files

--- src/main/java/mk/ukim/finki/brainboost/service/FavouritesListService.java
+++ src/main/java/mk/ukim/finki/brainboost/service/FavouritesListService.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface FavouritesListService {
         List<Course> listAllCoursesInFavouritesList(Integer id);
         FavouritesList getActiveFavouriteList(String username);
+        void addCourseToFavouritesList(String username, Long courseId);
+        FavouritesList removeCourseFromWishList(String userId, Integer productId);
 }
--- src/main/java/mk/ukim/finki/brainboost/service/impl/FavouritesListServiceImpl.java
+++ src/main/java/mk/ukim/finki/brainboost/service/impl/FavouritesListServiceImpl.java
@@ -4,23 +4,32 @@ import mk.ukim.finki.brainboost.domain.Course;
 import mk.ukim.finki.brainboost.domain.FavouritesList;
 import mk.ukim.finki.brainboost.domain.User;
 import mk.ukim.finki.brainboost.domain.enumerations.FavouriteListStatus;
+import mk.ukim.finki.brainboost.domain.exceptions.CourseAlreadyInFavouritesListException;
+import mk.ukim.finki.brainboost.domain.exceptions.CourseNotFoundException;
 import mk.ukim.finki.brainboost.domain.exceptions.FavouritesListNotFoundException;
 import mk.ukim.finki.brainboost.domain.exceptions.UserNotFoundException;
+import mk.ukim.finki.brainboost.repository.CourseRepository;
 import mk.ukim.finki.brainboost.repository.FavouritesListRepository;
 import mk.ukim.finki.brainboost.repository.UserRepository;
+import mk.ukim.finki.brainboost.service.CourseService;
 import mk.ukim.finki.brainboost.service.FavouritesListService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class FavouritesListServiceImpl implements FavouritesListService {
     private final FavouritesListRepository favouritesListRepository;
     private final UserRepository userRepository;
+    private final CourseRepository courseRepository;
+    private final CourseService courseService;
 
-    public FavouritesListServiceImpl(FavouritesListRepository favouritesListRepository, UserRepository userRepository) {
+    public FavouritesListServiceImpl(FavouritesListRepository favouritesListRepository, UserRepository userRepository, CourseRepository courseRepository, CourseService courseService) {
         this.favouritesListRepository = favouritesListRepository;
         this.userRepository = userRepository;
+        this.courseRepository = courseRepository;
+        this.courseService = courseService;
     }
 
     @Override
@@ -41,5 +50,27 @@ public class FavouritesListServiceImpl implements FavouritesListService {
                     FavouritesList favouritesList = new FavouritesList(user);
                     return this.favouritesListRepository.save(favouritesList);
                 });
+    }
+
+    @Override
+    public void addCourseToFavouritesList(String username, Long courseId) {
+        FavouritesList favouritesList = this.getActiveFavouriteList(username);
+        Course course = this.courseService.findById(courseId).orElseThrow(() -> new CourseNotFoundException(courseId));
+
+        if (favouritesList.getCourses()
+                .stream().filter(i -> i.getId().equals(courseId))
+                .toList().size() > 0)
+            throw new CourseAlreadyInFavouritesListException(courseId, username);
+        favouritesList.getCourses().add(course);
+        this.favouritesListRepository.save(favouritesList);
+    }
+
+    @Override
+    public FavouritesList removeCourseFromWishList(String userId, Integer courseId) {
+        FavouritesList favouritesList=this.getActiveFavouriteList(userId);
+        favouritesList.setCourses(favouritesList.getCourses()
+                .stream().filter(course -> !course.getId().equals(courseId))
+                .collect(Collectors.toList()));
+        return this.favouritesListRepository.save(favouritesList);
     }
 }
